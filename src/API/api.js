b981b29ebeab93441430f72fe4c70b332eebd017
Prashant-kumar-pcpk <prashantkumar.pcpk@gmail.com>
2026-05-06 00:31:51 +0530

    update backend url

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:9090/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://be-blogging-platform-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
